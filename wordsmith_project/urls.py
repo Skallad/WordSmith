7dc77c8b57047326939258ca0e2fe2a52baffb27
Skallad <gramatikofski.elena@gmail.com>
2022-11-12 17:08:19 +0000

    Try different url in urls.py

--- wordsmith_project/urls.py
+++ wordsmith_project/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('/admin/', admin.site.urls),
-    path('/summernote/', include('django_summernote.urls')),
+    path('wordsmith-eg.herokuapp.com/admin/', include('admin.site.urls')),
+    path('wordsmith-eg.herokuapp.com/summernote/', include('django_summernote.urls')),
 ]
