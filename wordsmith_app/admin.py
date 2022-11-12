4ba09f8be503c61388402c942a122eefa33ffd40
Skallad <gramatikofski.elena@gmail.com>
2022-11-12 02:03:08 +0000

    Add admin post and comment control and comment approval

--- wordsmith_app/admin.py
+++ wordsmith_app/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'author', 'created_on', 'status', 'slug')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('commentator', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('commentator', 'body')
+    action = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
